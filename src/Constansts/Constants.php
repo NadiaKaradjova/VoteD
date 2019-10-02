81babfb6015430b3f284efae8d17392edbb5f0b0
nadezhda <nadia_karadjova@abv.bg>
2019-10-02 16:48:23 +0300

    config

--- src/Constansts/Constants.php
+++ src/Constansts/Constants.php
@@ -10,10 +10,12 @@ class Constants
     const SHORTNAME = ["edt", "edt", "edp", "pdt", "parfum", "nail", "E d T", "EDC", "Lipstick", "Perfume", "Eyeliner", "Powder", "Lipliner",
         "Feather Hair Extensions", "Rouge", "Mascara", "Deodorant", "Candle"];
 
-    const DESCRIPTION = ["CHANEL", "MAYBELLINE", "D&G", "SISLEY", "YSL", "VERSACE", "GUERLAIN", "AMAVITA", "COOP VITALITY",
+    const DESCRIPTION = [
+        "CHANEL", "MAYBELLINE", "D&G", "SISLEY", "YSL", "VERSACE", "GUERLAIN", "AMAVITA", "COOP VITALITY",
         "DAVIDOFF", "CLARINS", "VICTORINOX", "GUESS", "BVLGARI", "GUCCI", "ADIDAS", "BALMAIN", "FERRAGAMO", "MEXX", "BOSS", "LANCOME",
         "MARC JACOBS", "MOSCHINO", "HERMES", "ARMANI", "FERRE", "CACHAREL", "MUGLER", "AZZARO", "POLICE", "LANVIN", "DIESEL",
         "ZEGNA", "DIOR", "ZIPPO", "JIMMY CHOO", "KENZO", "CARTIER", "HILFIGER", "BENETTON", "PRADA", "VALENTINO",
-        "EBNAT", "KYBUN", "KILIAN", "OREAL"];
+        "EBNAT", "KYBUN", "KILIAN", "OREAL", "DEBORAH", "GIVENCHY", "MAVALA", "PUPA", "LAUDER"
+    ];
 
 }
